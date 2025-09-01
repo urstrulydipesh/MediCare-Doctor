d43c7c3ee2962659eac75b4830892f056610753d
urstrulydipesh <dipesh.shah546@gmail.com>
2025-09-01 19:44:31 +0530

    specialiaztion

---
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+
+const Profile = () => {
+
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    phone:'',
+    bio:'',
+    gender:'',
+    specialization:'',
+    ticketPrice:null, 
+  })
+
+  const handleInputChange = e => {
+
+  }
+
+  return (
+    <div>
+      <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
+        Profile Information
+      </h2>
+
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Name*</p>
+          <input 
+            type="text" 
+            name="name" 
+            value={formData.name} 
+            onChange={handleInputChange} 
+            placeholder="Enter your name"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email*</p>
+          <input 
+            type="email" 
+            name="email" 
+            value={formData.email} 
+            onChange={handleInputChange} 
+            placeholder="Email address"
+            className="form__input"
+            readOnly
+            aria-readonly
+            disabled="true"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Phone*</p>
+          <input 
+            type="number" 
+            name="phone" 
+            value={formData.phone} 
+            onChange={handleInputChange} 
+            placeholder="Phone number"
+            className="form__input"
+          />
+        </div> 
+        <div className="mb-5">
+          <p className="form__label">Bio*</p>
+          <input 
+            type="text" 
+            name="bio" 
+            value={formData.bio} 
+            onChange={handleInputChange} 
+            placeholder="Bio "
+            className="form__input"
+            maxLength={100}
+          />
+        </div>
+
+        <div className="mb-5">
+  <div className="grid grid-cols-3 gap-5 mb-[30px]">
+    <div>
+      <p className="form__label">Gender*</p>
+      <select 
+        name="gender" 
+        value={formData.gender} 
+        onChange={handleInputChange} 
+        className="form__input py-3.5"
+      >
+        <option value="">Select</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+    </div>
+    <div>
+    <p className="form__label">Specialization*</p>
+      <select 
+        name="specialization" 
+        value={formData.specialization} 
+        onChange={handleInputChange} 
+        className="form__input py-3.5"
+      >
+        <option value="">Select</option>
+        <option value="surgeon">Surgeon</option>
+        <option value="neurologist">Neurologist</option>
+        <option value="dermatologist">Dermatologist</option>
+      </select>
+  </div>
+</div>
+</div>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,12 +1,12 @@
 import React from 'react' ;
 import { formatDate } from '../../utils/formatDate'
 
-const DoctorAbout = () => {
+const DoctorAbout = ({name, about, qualifications, experiences}) => {
   return (
     <div>
         <div>
             <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
-                About Of
+                About of
                 <span className='text-irisBlueColor font-bold text-[24px] leading-9'>
                     Dipesh Shah
                 </span>
