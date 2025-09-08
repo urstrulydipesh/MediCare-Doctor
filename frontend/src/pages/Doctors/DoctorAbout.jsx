5eaad4be35586ea1de9b61a08e6e69cdf2e20dbd
urstrulydipesh <dipesh.shah546@gmail.com>
2025-09-09 00:39:12 +0530

    changed invalid dates

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -17,7 +17,7 @@ const DoctorAbout = ({name, about, qualifications, experiences}) => {
 
         <div className='mt-12'>
             <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
-                Education
+                Qualifications
             </h3>
             <ul className='pt-4 md:p-5'>
 
@@ -39,14 +39,14 @@ const DoctorAbout = ({name, about, qualifications, experiences}) => {
 
         <div className='mt-12'>
             <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
-                Experience
+                Experiences
             </h3>
 
             <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
 
         {experiences?.map((item, index) => <li key={index} className='p-4 rounded bg-[#fff9ea]'>
                     <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
-                    {formatDate(item.startingData)} - {formatDate(item.endingData )}
+                    {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
                     </span>
                     <p className='text-[16px] leading-6 font-medium text-textColor'>
                             {item.position}
